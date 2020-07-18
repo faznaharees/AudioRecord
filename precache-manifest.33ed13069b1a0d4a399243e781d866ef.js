c8f307ff1daa928a1152233eb62e9834079f1d4d
Fazna <faznaharees@cet.ac.in>
2020-07-18 21:43:26 +0530

    Updates

---
+++ precache-manifest.33ed13069b1a0d4a399243e781d866ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d760f6e215802e6a776f81e458d0c972",
+    "url": "/AudioRecord/index.html"
+  },
+  {
+    "revision": "01034572e35f6e183fcd",
+    "url": "/AudioRecord/static/css/main.2481c5c4.chunk.css"
+  },
+  {
+    "revision": "ac3c7d72a114b5e15c29",
+    "url": "/AudioRecord/static/js/2.4631b654.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/AudioRecord/static/js/2.4631b654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01034572e35f6e183fcd",
+    "url": "/AudioRecord/static/js/main.b327c901.chunk.js"
+  },
+  {
+    "revision": "ca04cfd462eefd076ccd",
+    "url": "/AudioRecord/static/js/runtime-main.4462c772.js"
+  }
+]);
